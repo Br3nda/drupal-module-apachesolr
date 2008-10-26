e2b665db26de0abe22f54098c55543ce41549dab
robertDouglass <robertDouglass>
2008-10-26 15:13:07 +0000

    Unifying communication with Luke handler. Using wt=json instead of XML/XPath. No longer guaranteed to work with Solr 1.2 (see project page for notes on this decision)

--- SolrPhpClient/Apache/Solr/Service.php
+++ SolrPhpClient/Apache/Solr/Service.php
@@ -270,6 +270,7 @@ class Apache_Solr_Service
   protected function _sendRawGet($url, $timeout = FALSE)
   {
     //$http_response_header is set by file_get_contents
+    $http_response_header = NULL;
     $response = new Apache_Solr_Response(@file_get_contents($url), $http_response_header, $this->_createDocuments, $this->_collapseSingleValueArrays);
 
     if ($response->getHttpStatus() != 200)
