a34b362b8ef576f74b0003d3746a67a40f952a53
pwolanin <pwolanin>
2009-03-18 20:30:48 +0000

    #400882 by mkalkbrenner, fix faceting bug due to static counter in method add_field

--- Solr_Base_Query.php
+++ Solr_Base_Query.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: Solr_Base_Query.php,v 1.1.4.20 2009/02/27 04:32:59 pwolanin Exp $
+// $Id: Solr_Base_Query.php,v 1.1.4.21 2009/03/18 20:30:48 pwolanin Exp $
 
 class Solr_Base_Query {
 
@@ -103,7 +103,7 @@ class Solr_Base_Query {
   function __construct($solr, $querypath, $filterstring, $sortstring) {
     $this->solr = $solr;
     $this->querypath = trim($querypath);
-    $this->filters = trim($filterstring); 
+    $this->filters = trim($filterstring);
     $this->solrsort = trim($sortstring);
     $this->id = ++self::$idCount;
     $this->parse_filters();
@@ -114,10 +114,7 @@ class Solr_Base_Query {
   }
 
   function add_field($field, $value) {
-    static $i = 0;
-    // Counter guarantees that added fields come at the end of the query,
-    // in order.
-    $this->fields[$i++] = array('#name' => $field, '#value' => trim($value));
+    $this->fields[] = array('#name' => $field, '#value' => trim($value));
   }
 
   public function get_fields() {
