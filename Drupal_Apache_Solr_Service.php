fd207878fee08d4a0ba64d7b28e9418aabbeaa82
pwolanin <pwolanin>
2009-06-05 12:34:36 +0000

    #480814 by mkalkbrenner and pwolanin, add more detail to logging on errors

--- Drupal_Apache_Solr_Service.php
+++ Drupal_Apache_Solr_Service.php
@@ -206,8 +206,14 @@ class Drupal_Apache_Solr_Service extends Apache_Solr_Service {
   protected function _sendRawGet($url, $timeout = FALSE) {
     list ($data, $headers) = $this->_makeHttpRequest($url, 'GET', array(), '', $timeout);
     $response = new Apache_Solr_Response($data, $headers, $this->_createDocuments, $this->_collapseSingleValueArrays);
-    if ($response->getHttpStatus() != 200) {
-      throw new Exception('"' . $response->getHttpStatus() . '" Status: ' . $response->getHttpStatusMessage());
+    $code = (int) $response->getHttpStatus();
+    if ($code != 200) {
+      $message = $response->getHttpStatusMessage();
+      if ($code >= 400) {
+        // Add details, like Solr's exception message.
+        $message .= $response->getRawResponse();
+      }
+      throw new Exception('"' . $code . '" Status: ' . $message);
     }
     return $response;
   }
@@ -221,8 +227,14 @@ class Drupal_Apache_Solr_Service extends Apache_Solr_Service {
     $request_headers = array('Content-Type' => $contentType);
     list ($data, $headers) = $this->_makeHttpRequest($url, 'POST', $request_headers, $rawPost, $timeout);
     $response = new Apache_Solr_Response($data, $headers, $this->_createDocuments, $this->_collapseSingleValueArrays);
-    if ($response->getHttpStatus() != 200) {
-      throw new Exception('"' . $response->getHttpStatus() . '" Status: ' . $response->getHttpStatusMessage());
+    $code = (int) $response->getHttpStatus();
+    if ($code != 200) {
+      $message = $response->getHttpStatusMessage();
+      if ($code >= 400) {
+        // Add details, like Solr's exception message.
+        $message .= $response->getRawResponse();
+      }
+      throw new Exception('"' . $code . '" Status: ' . $message);
     }
     return $response;
   }
