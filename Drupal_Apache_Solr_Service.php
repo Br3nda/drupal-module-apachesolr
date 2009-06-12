83a94cdd707fe4d3a8573fe13402b4bd9333930e
pwolanin <pwolanin>
2009-06-12 18:16:31 +0000

    #489654 by JacobSingh, and pwolanin, allow users to set their index as 'read only'.

--- Drupal_Apache_Solr_Service.php
+++ Drupal_Apache_Solr_Service.php
@@ -224,6 +224,9 @@ class Drupal_Apache_Solr_Service extends Apache_Solr_Service {
    * @see Apache_Solr_Service::_sendRawGet()
    */
   protected function _sendRawPost($url, $rawPost, $timeout = FALSE, $contentType = 'text/xml; charset=UTF-8') {
+    if (variable_get('apachesolr_read_only', 0)) {
+      throw new Exception('Operating in read-only mode; updates are disabled.');
+    }
     $request_headers = array('Content-Type' => $contentType);
     list ($data, $headers) = $this->_makeHttpRequest($url, 'POST', $request_headers, $rawPost, $timeout);
     $response = new Apache_Solr_Response($data, $headers, $this->_createDocuments, $this->_collapseSingleValueArrays);
