ec77f725a601fa07550b8b6ab7ccb6bef0536eb7
jpmckinney <jpmckinney>
2010-06-16 14:24:34 +0000

    #828024 by jpmckinney: Issue after upgrading from 6.2x-beta to 6.2x-dev (jun. 12) version

--- Solr_Base_Query.php
+++ Solr_Base_Query.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: Solr_Base_Query.php,v 1.17 2010/06/11 16:22:12 jpmckinney Exp $
+// $Id: Solr_Base_Query.php,v 1.18 2010/06/16 14:24:34 jpmckinney Exp $
 
 class Solr_Base_Query implements Drupal_Solr_Query_Interface {
 
@@ -433,7 +433,7 @@ class Solr_Base_Query implements Drupal_Solr_Query_Interface {
             $subqueries[] = $value;
           }
         }
-        $fq['subqueries'][$id] =  " {$data['#q_operator']} (" . implode(" $operator " , $subqueries) . ")";
+        $fq['subqueries'][$id] =  " (" . implode(" $operator " , $subqueries) . ")";
       }
     }
     return $fq;
