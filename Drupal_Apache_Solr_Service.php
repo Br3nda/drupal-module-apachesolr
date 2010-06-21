959bbeff541352fb892cd4712ce1de994eb2ef52
pwolanin <pwolanin>
2010-06-21 19:29:13 +0000

    #830976 by eosrei, pwolanin: make sure we return a non-zero ping time on success.

--- Drupal_Apache_Solr_Service.php
+++ Drupal_Apache_Solr_Service.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: Drupal_Apache_Solr_Service.php,v 1.1.2.20.2.10 2010/05/03 18:50:27 jpmckinney Exp $
+// $Id: Drupal_Apache_Solr_Service.php,v 1.1.2.20.2.11 2010/06/21 19:29:13 pwolanin Exp $
 
 require_once 'SolrPhpClient/Apache/Solr/Service.php';
 
@@ -46,7 +46,8 @@ class Drupal_Apache_Solr_Service extends Apache_Solr_Service {
     $response = new Apache_Solr_Response($data, $headers);
 
     if ($response->getHttpStatus() == 200) {
-      return microtime(TRUE) - $start;
+      // Add 0.1 ms to the ping time so we never return 0.0.
+      return microtime(TRUE) - $start + 0.0001;
     }
     else {
       return FALSE;
